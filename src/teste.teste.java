32aa9843eb6d7c7d55d28d03edff195d26438569
GeisaSilvaGit <geisa_mb@yahoo.com.br>
2023-04-15 15:18:39 -0300

    exercicios metodos

--- src/teste.teste.java
+++ src/teste.teste.java
@@ -2,7 +2,7 @@ public class teste.teste {
     
         public static void main(String[] args) throws Exception {
         String palavra = JOptionPane.showInputDialog("Digite uma palavra");
-        System.out.println (palavra); //sysout + tab
+       
     }
 }
 }
